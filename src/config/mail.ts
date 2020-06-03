c736f08558e4fca7c10887cf31d95dca6a5ded32
Patrik Oliveira <patrik.oliveira@rarolabs.com.br>
2020-06-02 22:29:49 -0300

    feat(Personalizando Para Producao): Envio de Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'patrikro.oliveira@gmail.com',
+      name: 'Patrik Oliveira',
+    },
+  },
+} as IMailConfig;
